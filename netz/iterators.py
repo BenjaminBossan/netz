2e803f19fe2cc127b1530db13a47f8a5afb092e3
Benjamin Bossan <benjamin.bossan@gmail.com>
2015-04-08 11:36:27 +0200

    Added PadBatchIterator to homogenize heterogeneous numpy array batches.

--- netz/iterators.py
+++ netz/iterators.py
@@ -2,6 +2,7 @@
 from __future__ import division
 
 from nolearn.lasagne import BatchIterator
+import numpy as np
 
 
 class MultipleInputsBatchIterator(BatchIterator):
@@ -27,5 +28,47 @@ class MultipleInputsBatchIterator(BatchIterator):
                 yb = None
             yield self.transform(Xb, yb)
 
-    def transform(self, Xb, yb):
-        return Xb, yb
+
+class PadBatchIterator(BatchIterator):
+    """Pads the rows of a numpy array to all have same length.
+
+    This should by applied in particular to heterogeneous numpy arrays
+    where rows don't all have the same length. Heterogeneous arrays
+    may only be sliced in the 0'th dimension -- after padding, it can
+    also be sliced in the 1st dimension.  You may also use a 2d array
+    and choose pad_all=True to append an end token to all lines.
+
+    Parameters
+    ----------
+    batch_size : int
+      The size of a batch
+
+    pad_token : anything (default=0)
+      The token to pad with.
+
+    pad_all : bool (default=True)
+      Whether to pad even the longest line by a token. Makes all rows
+      1 longer.
+
+    yields
+    ------
+    Xb : numpy array
+      A padded batch of X.
+
+    yb : numpy array
+      An untransformed batch of y.
+
+    """
+    def __init__(self, batch_size, pad_token=0, pad_all=True):
+        self.batch_size = batch_size
+        self.pad_token = pad_token
+        self.pad_all = pad_all
+
+    def transform(self, X, y):
+        max_len = max(map(len, X))
+        Xt = []
+        for x in X:
+            num_pad = max_len - len(x) + int(self.pad_all)
+            padded_x = x + num_pad * [self.pad_token]
+            Xt.append(np.array(padded_x))
+        return np.asarray(Xt, dtype=X.dtype), y
