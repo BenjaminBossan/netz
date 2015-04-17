91f6c951c5fb6bb36f866e785a6c3ee6ad5ada03
Benjamin Bossan <benjamin.bossan@gmail.com>
2015-04-17 14:57:31 +0200

    An output layer for regression.

--- netz/layers/output.py
+++ netz/layers/output.py
@@ -5,22 +5,34 @@ import numpy as np
 from theano.tensor.shared_randomstreams import RandomStreams
 
 from ..nonlinearities import softmax
+from ..nonlinearities import identity
 from .dense import DenseLayer
 
 srng = RandomStreams(seed=17411)
 
-__all__ = ['OutputLayer']
+__all__ = ['OutputLayer', 'RegressionOutputLayer']
 
 
 class OutputLayer(DenseLayer):
     def __init__(self, num_units=None, num_features=None, nonlinearity=softmax,
                  *args, **kwargs):
-        super(DenseLayer, self).__init__(*args, **kwargs)
+        super(OutputLayer, self).__init__(num_units, num_features,
+                                          *args, **kwargs)
         self.nonlinearity = nonlinearity
-        self.num_units = num_units
-        self.num_features = num_features
 
     def initialize(self, X, y):
         self.num_units = (self.num_units if self.num_units
                           else len(np.unique(y)))
         super(OutputLayer, self).initialize(X, y)
+
+
+class RegressionOutputLayer(DenseLayer):
+    def __init__(self, num_units=None, num_features=None,
+                 nonlinearity=identity, *args, **kwargs):
+        super(RegressionOutputLayer, self).__init__(num_units, num_features,
+                                                    *args, **kwargs)
+        self.nonlinearity = nonlinearity
+
+    def initialize(self, X, y):
+        self.num_units = self.num_units if self.num_units else 1
+        super(RegressionOutputLayer, self).initialize(X, y)
